f282f13ba29a9939738800643ffab8642d52164c
Johan Girod <johangirod@gmail.com>
2016-04-06 19:31:30 +0200

    Add changing sentences

--- js/main.js
+++ js/main.js
@@ -8,3 +8,26 @@
 })(jQuery);
 
 
+(function($) {
+    var sentences = [
+        "Nous ne rentrerons pas chez nous ce soir",
+        "Ni loi, ni travail",
+        "Un joyeux bordel est possible",
+        "Le jour : à bout ; la nuit : debout",
+        "Nos rêves ne rentrent pas dans vos urnes",
+    ]
+    var counter = 0;
+    setInterval(function() {
+        var container = $('.nd_header__quote');
+        container
+            .children()
+            .fadeOut()
+            .remove();
+        container
+            .append($('<span>"' + sentences[counter] + '"</span>').fadeIn())
+        counter++;
+        if (counter === sentences.length) {
+            counter = 0;
+        }
+    }, 10000);
+})(jQuery);
