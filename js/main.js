498963913d3d57d9a4f01eba2995db6a97515226
Fadomire <fadomire@gmail.Com>
2016-04-07 16:00:55 +0200

    facebook and twitter cards

--- js/main.js
+++ js/main.js
@@ -9,19 +9,15 @@
 
     // quote rotation
     var sentences = [
-        "Nous ne rentrerons pas chez nous ce soir",
-        "Ni loi, ni travail",
-        "Un joyeux bordel est possible",
-        "Le jour : à bout ; la nuit : debout",
-        "Nos rêves ne rentrent pas dans vos urnes",
+        // "Nous ne rentrerons pas chez nous ce soir",
+        // "Ni loi, ni travail",
+        // "Un joyeux bordel est possible",
+        // "Le jour : à bout ; la nuit : debout",
+        // "Nos rêves ne rentrent pas dans vos urnes",
     ]
     var counter = 0;
     setInterval(function() {
         var container = $('.nd_header__quote');
-        container
-            .children()
-            .fadeOut()
-            .remove();
         container
             .append($('<span>"' + sentences[counter] + '"</span>').fadeIn())
         counter++;
@@ -57,7 +53,40 @@
 
 
       }
-    })
+    });
+
+
+    // get facebook feed
+    $.ajax({
+      url: 'http://localhost:3000/api/facebook',
+      success: function (resp, status, jqxhr) {
+
+        var filteredPost = _.reject(resp.data, function (val){ 
+          return !val.message && !val.caption; 
+        });
+
+        
+
+        $('#news .card').each(function (index, value) {
+          
+          $(value).find('.card-content').text(filteredPost[index].message || filteredPost[index].caption);
+          $(value).find('.card-image').html('<img src="'+filteredPost[index].full_picture+'"/>');
+
+        });
+
+      }
+    });
+
+
+    // get twitter feed
+    $.ajax({
+      url: 'http://localhost:3000/api/twitter',
+      success: function (resp, status, jqxhr) {
+        _.each(resp, function (element, index, list) {
+          sentences.push(element.text)
+        })
+      }
+    });
 
 
 })(jQuery);
